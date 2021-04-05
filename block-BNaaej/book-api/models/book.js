267daf3e82ce525a94bcbaed2b552c106662a572
Abhishek Kumar <abhivision3@gmail.com>
2021-04-05 23:01:37 +0530

    refactor assignment 1

---
+++ block-BNaaej/book-api/models/book.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var bookSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    author: String,
+    price: Number,
+    page: Number,
+    category: [String],
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+  },
+  { timestamps: true }
+);
+
+var Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
